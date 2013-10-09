80dda68787015bab8d3b5e7c2dda2ee32809c02f
Junerey Casuga <zeresoft@zeresoft-desktop.(none)>
2013-10-09 12:50:41 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StalkyRails::Application.config.secret_key_base = 'e0b8e92732dc84b5eaade790a7e7a1e746cb5139fb5c9589f5d29963d8a26fa6371d73256a66706bc8db8443332343606ae0ceb4b69b5396d339013f1707a4fa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StalkyRails::Application.config.session_store :cookie_store, key: '_stalky-rails_session'
